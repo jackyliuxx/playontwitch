b6123249adaeccf17e95c676bb03d9a8f88a2284
jackyliuxx <jackyliuxx@gmail.com>
2015-03-22 18:06:31 +0800

    add config file and log function

--- presskey_gba.py
+++ presskey_gba.py
@@ -14,42 +14,10 @@ mp = {
         'START' : 'Return'
         }
 
-mpk = ['UP', 'DOWN','LEFT','RIGHT',
+command = ['UP', 'DOWN','LEFT','RIGHT',
         'A', 'B', 'L','R',
         'SELECT', 'START']
 
-def _press(username, key):
-    print(username, ':', key)
-    sp.call('xdotool search --onlyvisible --name VBA-M key --delay 200 %s' % mp[key], shell=True)
-    # time.sleep(0.1)
+def press(key, delay):
+    sp.call('xdotool search --onlyvisible --name VBA-M key --delay %d %s' % (delay, mp[key]), shell=True)
 
-def press(p, mode):
-    username = p['username']
-    msg = p['message']
-    msg = msg.lstrip()
-    if mode == 0:
-        if msg in mpk:
-            _press(username, msg)
-            return True
-        else:
-            return False
-    elif mode == 1:
-        op = []
-        kg = 1
-        while len(msg) and kg:
-            kg = 0
-            for i in mpk:
-                if len(i) > len(msg):
-                    continue
-                if i == msg[:len(i)]:
-                    op.append(i)
-                    msg = msg[len(i):].lstrip()
-                    kg = 1
-                    break
-            if len(op)>6:
-                return False
-        if kg == 0:
-            return False
-        for i in op:
-            _press(username, i)
-        return True
